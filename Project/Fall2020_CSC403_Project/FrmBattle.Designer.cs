4d24f10a325616d06017ef84756d907e4f58ffb8
MOE116 <145284139+MOE116@users.noreply.github.com>
2023-10-29 17:46:41 -0500

    Commit after Merge for Moses and Likith features

--- Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmBattle.Designer.cs
@@ -33,9 +33,9 @@
             this.picEnemy = new System.Windows.Forms.PictureBox();
             this.picPlayer = new System.Windows.Forms.PictureBox();
             this.tmrFinalBattle = new System.Windows.Forms.Timer(this.components);
-
             this.FleeButton = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Healbtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBossBattle)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picEnemy)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
@@ -44,11 +44,10 @@
             // btnAttack
             // 
             this.btnAttack.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-
-            this.btnAttack.Location = new System.Drawing.Point(169, 519);
-            this.btnAttack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAttack.Location = new System.Drawing.Point(190, 649);
+            this.btnAttack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAttack.Name = "btnAttack";
-            this.btnAttack.Size = new System.Drawing.Size(171, 53);
+            this.btnAttack.Size = new System.Drawing.Size(192, 66);
             this.btnAttack.TabIndex = 2;
             this.btnAttack.Text = "Attack";
             this.btnAttack.UseVisualStyleBackColor = true;
@@ -59,33 +58,30 @@
             this.lblPlayerHealthFull.BackColor = System.Drawing.Color.Blue;
             this.lblPlayerHealthFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPlayerHealthFull.ForeColor = System.Drawing.Color.White;
-            this.lblPlayerHealthFull.Location = new System.Drawing.Point(95, 74);
+            this.lblPlayerHealthFull.Location = new System.Drawing.Point(107, 92);
             this.lblPlayerHealthFull.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPlayerHealthFull.Name = "lblPlayerHealthFull";
-            this.lblPlayerHealthFull.Size = new System.Drawing.Size(301, 25);
+            this.lblPlayerHealthFull.Size = new System.Drawing.Size(339, 31);
             this.lblPlayerHealthFull.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label1.Location = new System.Drawing.Point(93, 73);
+            this.label1.Location = new System.Drawing.Point(105, 91);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(305, 28);
-
+            this.label1.Size = new System.Drawing.Size(343, 35);
             this.label1.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.BackColor = System.Drawing.Color.Black;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-
-            this.label2.Location = new System.Drawing.Point(687, 73);
+            this.label2.Location = new System.Drawing.Point(773, 91);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(305, 28);
-
+            this.label2.Size = new System.Drawing.Size(343, 35);
             this.label2.TabIndex = 5;
             // 
             // lblEnemyHealthFull
@@ -93,23 +89,20 @@
             this.lblEnemyHealthFull.BackColor = System.Drawing.Color.Blue;
             this.lblEnemyHealthFull.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEnemyHealthFull.ForeColor = System.Drawing.Color.White;
-
-
-            this.lblEnemyHealthFull.Location = new System.Drawing.Point(688, 74);
+            this.lblEnemyHealthFull.Location = new System.Drawing.Point(774, 92);
             this.lblEnemyHealthFull.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEnemyHealthFull.Name = "lblEnemyHealthFull";
-            this.lblEnemyHealthFull.Size = new System.Drawing.Size(301, 25);
-
+            this.lblEnemyHealthFull.Size = new System.Drawing.Size(339, 31);
             this.lblEnemyHealthFull.TabIndex = 6;
             // 
             // picBossBattle
             // 
             this.picBossBattle.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.battle_screen;
             this.picBossBattle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.picBossBattle.Location = new System.Drawing.Point(1040, 693);
-            this.picBossBattle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picBossBattle.Location = new System.Drawing.Point(1170, 866);
+            this.picBossBattle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.picBossBattle.Name = "picBossBattle";
-            this.picBossBattle.Size = new System.Drawing.Size(40, 34);
+            this.picBossBattle.Size = new System.Drawing.Size(45, 42);
             this.picBossBattle.TabIndex = 7;
             this.picBossBattle.TabStop = false;
             this.picBossBattle.Visible = false;
@@ -120,10 +113,10 @@
             this.picEnemy.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.enemy_koolaid;
             this.picEnemy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.picEnemy.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picEnemy.Location = new System.Drawing.Point(687, 121);
-            this.picEnemy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picEnemy.Location = new System.Drawing.Point(773, 151);
+            this.picEnemy.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.picEnemy.Name = "picEnemy";
-            this.picEnemy.Size = new System.Drawing.Size(304, 328);
+            this.picEnemy.Size = new System.Drawing.Size(342, 409);
             this.picEnemy.TabIndex = 1;
             this.picEnemy.TabStop = false;
             // 
@@ -133,10 +126,10 @@
             this.picPlayer.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.player;
             this.picPlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.picPlayer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.picPlayer.Location = new System.Drawing.Point(93, 121);
-            this.picPlayer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picPlayer.Location = new System.Drawing.Point(105, 151);
+            this.picPlayer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.picPlayer.Name = "picPlayer";
-            this.picPlayer.Size = new System.Drawing.Size(304, 328);
+            this.picPlayer.Size = new System.Drawing.Size(342, 409);
             this.picPlayer.TabIndex = 0;
             this.picPlayer.TabStop = false;
             // 
@@ -148,34 +141,45 @@
             // FleeButton
             // 
             this.FleeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FleeButton.Location = new System.Drawing.Point(413, 649);
+            this.FleeButton.Location = new System.Drawing.Point(190, 724);
+            this.FleeButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.FleeButton.Name = "FleeButton";
-            this.FleeButton.Size = new System.Drawing.Size(195, 66);
+            this.FleeButton.Size = new System.Drawing.Size(192, 61);
             this.FleeButton.TabIndex = 8;
             this.FleeButton.Text = "Flee";
             this.FleeButton.UseVisualStyleBackColor = true;
-            this.FleeButton.Click += new System.EventHandler(this.FleeButton_Click); 
+            this.FleeButton.Click += new System.EventHandler(this.FleeButton_Click);
             // 
-            // button2
+            // button1
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.button2.Location = new System.Drawing.Point(169, 603);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(170, 53);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "HEAL";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Healbtn
+            // 
+            this.Healbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.Healbtn.Location = new System.Drawing.Point(190, 801);
+            this.Healbtn.Name = "Healbtn";
+            this.Healbtn.Size = new System.Drawing.Size(192, 56);
+            this.Healbtn.TabIndex = 10;
+            this.Healbtn.Text = "HEAL";
+            this.Healbtn.UseVisualStyleBackColor = true;
+            this.Healbtn.Click += new System.EventHandler(this.Healbtn_Click);
             // 
             // FrmBattle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Green;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1096, 742);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.FleeButton);      
+            this.ClientSize = new System.Drawing.Size(1233, 928);
+            this.Controls.Add(this.Healbtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FleeButton);
             this.Controls.Add(this.picBossBattle);
             this.Controls.Add(this.lblEnemyHealthFull);
             this.Controls.Add(this.label2);
@@ -185,7 +189,7 @@
             this.Controls.Add(this.picEnemy);
             this.Controls.Add(this.picPlayer);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmBattle";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Fight!";
@@ -208,7 +212,7 @@
     private System.Windows.Forms.PictureBox picBossBattle;
     private System.Windows.Forms.Timer tmrFinalBattle;
         private System.Windows.Forms.Button FleeButton;
-
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Healbtn;
     }
 }
--- Project/Fall2020_CSC403_Project/FrmBattle.cs
+++ Project/Fall2020_CSC403_Project/FrmBattle.cs
@@ -6,103 +6,116 @@ using System.Drawing.Printing;
 using System.Media;
 using System.Windows.Forms;
 
-namespace Fall2020_CSC403_Project {
-  public partial class FrmBattle : Form {
-    public static FrmBattle instance = null;
-    private Enemy enemy;
-    private Player player;
+namespace Fall2020_CSC403_Project
+{
+    public partial class FrmBattle : Form
+    {
+        public static FrmBattle instance = null;
+        private Enemy enemy;
+        private Player player;
         private int healCount = 0;   //Heal count for heal button added
 
-        private FrmBattle() {
-      InitializeComponent();
-      player = Game.player;
-    }
+        private FrmBattle()
+        {
+            InitializeComponent();
+            player = Game.player;
+        }
 
-    public void Setup() {
-      // update for this enemy
-      picEnemy.BackgroundImage = enemy.Img;
-      picEnemy.Refresh();
-      BackColor = enemy.Color;
-      picBossBattle.Visible = false;
+        public void Setup()
+        {
+            // update for this enemy
+            picEnemy.BackgroundImage = enemy.Img;
+            picEnemy.Refresh();
+            BackColor = enemy.Color;
+            picBossBattle.Visible = false;
 
-      // Observer pattern
-      enemy.AttackEvent += PlayerDamage;
-      player.AttackEvent += EnemyDamage;
+            // Observer pattern
+            enemy.AttackEvent += PlayerDamage;
+            player.AttackEvent += EnemyDamage;
 
-      // show health
-      UpdateHealthBars();
-    }
+            // show health
+            UpdateHealthBars();
+        }
 
-    public void SetupForBossBattle() {
-    
-      picBossBattle.Location = Point.Empty;
-      picBossBattle.Size = ClientSize;
-      picBossBattle.Visible = true;
+        public void SetupForBossBattle()
+        {
 
-      MusicSettings.StopBackgroundMusic();
-      SoundPlayer simpleSound = new SoundPlayer(Resources.final_battle);
-      simpleSound.Play();
-      //MusicSettings.PlayBackgroundMusic();
-      tmrFinalBattle.Enabled = true;
-    }
+            picBossBattle.Location = Point.Empty;
+            picBossBattle.Size = ClientSize;
+            picBossBattle.Visible = true;
 
-    public static FrmBattle GetInstance(Enemy enemy) {
-      if (instance == null) {
-        instance = new FrmBattle();
-        instance.enemy = enemy;
-        instance.Setup();
-      }
-      return instance;
-    }
+            MusicSettings.StopBackgroundMusic();
+            SoundPlayer simpleSound = new SoundPlayer(Resources.final_battle);
+            simpleSound.Play();
+            //MusicSettings.PlayBackgroundMusic();
+            tmrFinalBattle.Enabled = true;
+        }
 
-    private void UpdateHealthBars() {
-      float playerHealthPer = player.Health / (float)player.MaxHealth;
-      float enemyHealthPer = enemy.Health / (float)enemy.MaxHealth;
+        public static FrmBattle GetInstance(Enemy enemy)
+        {
+            if (instance == null)
+            {
+                instance = new FrmBattle();
+                instance.enemy = enemy;
+                instance.Setup();
+            }
+            return instance;
+        }
 
-      const int MAX_HEALTHBAR_WIDTH = 226;
-      lblPlayerHealthFull.Width = (int)(MAX_HEALTHBAR_WIDTH * playerHealthPer);
-      lblEnemyHealthFull.Width = (int)(MAX_HEALTHBAR_WIDTH * enemyHealthPer);
+        private void UpdateHealthBars()
+        {
+            float playerHealthPer = player.Health / (float)player.MaxHealth;
+            float enemyHealthPer = enemy.Health / (float)enemy.MaxHealth;
 
-      lblPlayerHealthFull.Text = player.Health.ToString();
-      lblEnemyHealthFull.Text = enemy.Health.ToString();
-    }
+            const int MAX_HEALTHBAR_WIDTH = 226;
+            lblPlayerHealthFull.Width = (int)(MAX_HEALTHBAR_WIDTH * playerHealthPer);
+            lblEnemyHealthFull.Width = (int)(MAX_HEALTHBAR_WIDTH * enemyHealthPer);
 
-    private void btnAttack_Click(object sender, EventArgs e) {
-      player.OnAttack(-4);
-      if (enemy.Health > 0) {
-        enemy.OnAttack(-2);
-      }
+            lblPlayerHealthFull.Text = player.Health.ToString();
+            lblEnemyHealthFull.Text = enemy.Health.ToString();
+        }
 
-      UpdateHealthBars();
-      if (player.Health <= 0 || enemy.Health <= 0) {
-        instance = null;
-        Close();
-      }
-    }
+        private void btnAttack_Click(object sender, EventArgs e)
+        {
+            player.OnAttack(-4);
+            if (enemy.Health > 0)
+            {
+                enemy.OnAttack(-2);
+            }
 
-    private void EnemyDamage(int amount) {
-      enemy.AlterHealth(amount);
-    }
+            UpdateHealthBars();
+            if (player.Health <= 0 || enemy.Health <= 0)
+            {
+                instance = null;
+                Close();
+            }
+        }
 
-    private void PlayerDamage(int amount) {
-      player.AlterHealth(amount);
-    }
+        private void EnemyDamage(int amount)
+        {
+            enemy.AlterHealth(amount);
+        }
 
-    private void tmrFinalBattle_Tick(object sender, EventArgs e) {
-      picBossBattle.Visible = false;
-      tmrFinalBattle.Enabled = false;
-      MusicSettings.PlayBackgroundMusic();
+        private void PlayerDamage(int amount)
+        {
+            player.AlterHealth(amount);
+        }
 
-    }
+        private void tmrFinalBattle_Tick(object sender, EventArgs e)
+        {
+            picBossBattle.Visible = false;
+            tmrFinalBattle.Enabled = false;
+            MusicSettings.PlayBackgroundMusic();
+
+        }
 
         private void FleeButton_Click(object sender, EventArgs e)
         {
             instance = null;
             Close();
         }
-    }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Healbtn_Click(object sender, EventArgs e)  //Player heal
         {
             if (healCount < 5)
             {
